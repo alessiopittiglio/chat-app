6a054e20592a66c43593a20f02d4e39a788f2c07
Alessio Pittiglio <alessiopittiglio@outlook.com>
2021-06-27 18:34:13 +0200

    First commit

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client'
+
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://chat-application-unicas.herokuapp.com/';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        var connectionOptions = {
+            "force new connection": true,
+            "reconnectionAttempts": "Infinity",
+            "timeout": 10000,
+            "transports": ["websocket"]
+        };
+
+        socket = io(ENDPOINT, connectionOptions);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if (error) {
+                alert(error);
+            }
+        });
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages(messages => [...messages, message]);
+        });
+    }, []);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    return (
+        <div className="container">
+            <InfoBar room={room} />
+            <Messages messages={messages} name={name} />
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        </div>
+    );
+}
+
+export default Chat;
